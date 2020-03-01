e0d5479c1ef5db0999aedfb8f66041ce3928c7c9
Nguyễn Văn Hoàng <nguyenvanhoang26041994@gmail.com>
2020-03-01 12:44:39 +0700

    Update distanceBetweenItem.js

--- Javascript/algorithm/distanceBetweenItem.js
+++ Javascript/algorithm/distanceBetweenItem.js
@@ -1,38 +1,22 @@
 
 let list = ['cat', 'dog', 'bird', 'fish', 'cat', 'duck', 'chicken', 'dog'];
-
 function distanceBetweenItem(arr, a, b) {
     let fa = undefined;
     let fb = undefined;
 
-    for (let i = 0; i < arr.length; i++) {
-        if (a === arr[i]) {
-            if (fa === undefined) {
-                fa = i;
-            } else {
-                if (fb === undefined) {
-                    fa = i;
-                } else {
-                    if (Math.abs(fb - i) < Math.abs(fb - fa)) {
-                        fa = i;
-                    }
-                }
-            }
+    for (let i = 0; i < list.length; i++) {
+        if ((a === list[i] && fa === undefined) ||
+            (a === list[i] && fa !== undefined && fb === undefined) ||
+            (a === list[i] && fa !== undefined && fb !== undefined && Math.abs(fb - i) < Math.abs(fb - fa))
+        ) {
+            fa = i;
         }
 
-        if (b === arr[i]) {
-            if (fb === undefined) {
-                fb = i;;
-            } else {
-                if (fa === undefined) {
-                    fb = i;
-                } else {
-                    if (Math.abs(i - fa) < Math.abs(fb - fa)) {
-                        fb = i;
-                    }
-                }
-            }
-            
+        if ((b === list[i] && fb === undefined) ||
+            (b === list[i] && fb !== undefined && fa === undefined) ||
+            (b === list[i] && fb !== undefined && fa !== undefined && Math.abs(i - fa) < Math.abs(fb - fa))
+        ) {
+            fb = i;
         }
     }
 
@@ -41,6 +25,6 @@ function distanceBetweenItem(arr, a, b) {
 
 distanceBetweenItem(list, 'dog', 'cat'); // 1
 distanceBetweenItem(list, 'dog', 'chicken'); // 1
-distanceBetweenItem(list, 'dog', 'duck'); // 4
+distanceBetweenItem(list, 'dog', 'duck'); // 2
 distanceBetweenItem(list, 'dog', 'dog'); // 0
 distanceBetweenItem(list, 'dog', 'dog2'); // -1
